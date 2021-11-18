2ff743d2026bf4a3e3d822f86c939fc8ec3a1754
Valentin Uchkunev <Valentin.Uchkunev@acronis.com>
2021-11-18 23:08:19 +0200

    fixed test - sort() used as value

--- selection_test.go
+++ selection_test.go
@@ -16,7 +16,8 @@ func Test_Sort(t *testing.T) {
 	unsorted := []int{5,4,3,1,2}
 	sorted := []int{1,2,3,4,5}
 
-	if !reflect.DeepEqual(sorted, Sort(unsorted)) {
-		t.Errorf("error sorting slice, got: %+v, expect: %+v", Sort(unsorted), sorted)
+	Sort(unsorted)
+	if !reflect.DeepEqual(sorted, unsorted) {
+		t.Errorf("error sorting slice, got: %+v, expect: %+v", unsorted, sorted)
 	}
 }
